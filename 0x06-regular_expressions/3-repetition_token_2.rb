0b4fe13df3b494d97e7a2d654d0ad4972c774dbc
Ab-Ruh <abell.demere@gmail.com>
2023-08-01 18:33:41 +0300

    regular

--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-# Check if an argument is provided
-if ARGV.length != 1
-    puts "Usage: #{$PROGRAM_NAME} <string>"
-    exit 1
-  end
-  
-  # Extract the argument from command line
-  input_string = ARGV[0]
-  
-  # Define the regular expression pattern with repetition token '#2'
-  pattern = /hbt*n/
-  
-  # Use the match method to find all occurrences of the pattern in the input string
-  matches = input_string.scan(pattern)
-  
-  # Output the matched string(s)
-  puts matches.join
-  
+puts ARGV[0].scan(/hbt+n/).join
--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-# Check if an argument is provided
-if ARGV.length != 1
-    puts "Usage: #{$PROGRAM_NAME} <phone_number>"
-    exit 1
-  end
-  
-  # Extract the argument from command line
-  phone_number = ARGV[0]
-  
-  # Define the regular expression pattern
-  pattern = /^\d{10}$/
-  
-  # Use the match method to check if the input string matches the pattern
-  match = phone_number.match(pattern)
-  
-  # Output the result
-  puts match ? match[0] : ""
-  
+^(/d{3})(/s|/-)?(/d{3})(/s|/-)?(/d{4})$
