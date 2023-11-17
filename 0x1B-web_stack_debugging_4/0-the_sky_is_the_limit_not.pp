aa417e1e6beebe2472c62f09b4f86b32b093cd7b
Ab-Ruh <abell.demere@gmail.com>
2023-11-17 21:51:56 +0300

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,10 @@
 # Amend Nginx worker processes to improve performance
-exec { 'fix--for-nginx':
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf && sudo service nginx restart",
-  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+exec { 'replace-the-limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/usr/local/bin', '/bin'],
+}
+# Reboot Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,11 @@
 # Increase the hard file limit for the 'holberton' user
-exec { 'replace-the-limit':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => ['/usr/local/bin', '/bin'],
+exec { 'limit-for-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
 }
 
 # Increase the soft file limit for the 'holberton' user
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+exec { 'soft-limit-for-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/',
 }
